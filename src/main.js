76e2c46632974a7f715b7400296454b2c494432b
Max Ruuen <max@madebyruuen.com>
2023-10-26 22:50:20 +1100

    Fix aria-expanded state, fix focus indicator on click Added focusVisible: false to the focus() calls on nav menu open/close Works on some browsers to not show a focus indicator on click Still maintains the indicator when using keyboard nav/SR nav Add comments to toggleMobileNav func for clarity

--- src/main.js
+++ src/main.js
@@ -21,15 +21,25 @@ navMenu.addEventListener("keydown", (e) => {
 function toggleMobileNav(action) {
     // TODO: Confirm if you should set expanded on both buttons that toggle the element
     if (action === "close") {
+        // Remove classes to show elements
         body.classList.remove("prevent-scroll");
         navMenu.classList.remove("navbar__menu--open");
+        // Update aria-expanded for nav menu buttons
+        btnOpenMenu.ariaExpanded = "false";
         btnCloseMenu.ariaExpanded = "false";
-        btnOpenMenu.focus({ preventScroll: true })
+        // Return focus to the open menu button (no indicator when clicked)
+        btnOpenMenu.focus({ preventScroll: true, focusVisible: false })
         return;
     }
 
+    // Add classes to show elements
     body.classList.add("prevent-scroll");
     navMenu.classList.add("navbar__menu--open");
+    // Update aria-expanded for nav menu buttons
     btnOpenMenu.ariaExpanded = "true";
-    firstNavLink.focus({ preventScroll: true })
+    btnCloseMenu.ariaExpanded = "true";
+    // Focus the first nav link
+    // "focusVisible: false" prevents visual indicator on focus when clicked for some browsers
+    // Focus indicator still appears when using tab/SR to toggle menu and navigate
+    firstNavLink.focus({ preventScroll: true, focusVisible: false })
 }
