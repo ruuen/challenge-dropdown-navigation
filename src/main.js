884fb59d1fba27fa0126003bdfd008f57034e707
Max Ruuen <max@madebyruuen.com>
2023-10-26 23:28:13 +1100

    Add dropdown show/hide toggling

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 const body = document.querySelector("body");
 const navMenu = document.querySelector(".navbar__menu");
-const firstNavLink = navMenu.querySelector(".navbar__link-group > *");
+const firstNavLink = navMenu.querySelector(".navbar__link-group > .navbar__dropdown > .navbar__dropdown-toggle");
 
 const btnOpenMenu = document.querySelector(`.navbar__btn[data-menu-action="open"]`);
 const btnCloseMenu = document.querySelector(`.navbar__btn[data-menu-action="close"]`);
+const dropdownBtns = document.querySelectorAll(".navbar__dropdown-toggle");
 
 btnOpenMenu.addEventListener("click", () => toggleMobileNav(btnOpenMenu.dataset.menuAction));
 btnCloseMenu.addEventListener("click", () => toggleMobileNav(btnCloseMenu.dataset.menuAction));
@@ -17,13 +18,15 @@ navMenu.addEventListener("keydown", (e) => {
         toggleMobileNav("close");
     }
 })
+dropdownBtns.forEach(btn => btn.addEventListener("click", (e) => toggleDropdown(e.currentTarget.getAttribute("aria-controls"))));
 
 function toggleMobileNav(action) {
+    const displayClass = "navbar__menu--open";
     // TODO: Confirm if you should set expanded on both buttons that toggle the element
     if (action === "close") {
         // Remove classes to show elements
         body.classList.remove("prevent-scroll");
-        navMenu.classList.remove("navbar__menu--open");
+        navMenu.classList.remove(displayClass);
         // Update aria-expanded for nav menu buttons
         btnOpenMenu.ariaExpanded = "false";
         btnCloseMenu.ariaExpanded = "false";
@@ -34,7 +37,7 @@ function toggleMobileNav(action) {
 
     // Add classes to show elements
     body.classList.add("prevent-scroll");
-    navMenu.classList.add("navbar__menu--open");
+    navMenu.classList.add(displayClass);
     // Update aria-expanded for nav menu buttons
     btnOpenMenu.ariaExpanded = "true";
     btnCloseMenu.ariaExpanded = "true";
@@ -42,4 +45,16 @@ function toggleMobileNav(action) {
     // "focusVisible: false" prevents visual indicator on focus when clicked for some browsers
     // Focus indicator still appears when using tab/SR to toggle menu and navigate
     firstNavLink.focus({ preventScroll: true, focusVisible: false })
+}
+
+function toggleDropdown(dropdownId) {
+    const displayClass = "navbar__dropdown--open";
+    const element = document.querySelector(`#${dropdownId}`).closest(".navbar__dropdown");
+
+    if (element.classList.contains(displayClass)) {
+        element.classList.remove(displayClass);
+        return;
+    }
+
+    element.classList.add(displayClass);
 }
